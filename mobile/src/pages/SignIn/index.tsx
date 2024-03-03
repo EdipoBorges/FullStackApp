b7d8628cf0bc2858774cec26a966a0d579178043
EdipoBorges <edipoborgesc@gmail.com>
2024-03-03 19:12:31 -0300

    logo_m

--- mobile/src/pages/SignIn/index.tsx
+++ mobile/src/pages/SignIn/index.tsx
@@ -1,12 +1,75 @@
-import React from "react";
-import { View, Text, } from "react-native";
+import React from 'react';
+import { View, Text, StyleSheet, Image, TextInput, TouchableOpacity } from 'react-native'
 
+export default function SignIn(){
+  return(
+    <View style={styles.container}>
+      <Image
+        style={styles.logo}
+        source={require('../../assets/logo_trial.png')}
+      />
 
-export default function SignIn() {
-    return (
-        <View>
-            <Text>Tela de Login</Text>
-        </View>
-    );
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder="Digite seu email"   
+          style={styles.input}     
+          placeholderTextColor="#F0F0F0"
+        />
+
+        <TextInput
+          placeholder="Sua Senha"      
+          style={styles.input}   
+          placeholderTextColor="#F0F0F0"
+          secureTextEntry={true}
+        />     
+
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Acessar</Text>
+        </TouchableOpacity>   
+      </View>
+
+    </View>
+  )
 }
 
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+    justifyContent:'center',
+    alignItems: 'center',
+    backgroundColor: '#1d2e24'
+  },
+  logo:{
+    marginBottom: 0,
+    resizeMode: "center"    
+  },
+  inputContainer:{
+    width: '95%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 32,
+    paddingHorizontal: 14,
+  },
+  input:{
+    width: '95%',
+    height: 40,
+    backgroundColor: '#102626',
+    marginBottom: 12,
+    borderRadius: 4,
+    paddingHorizontal: 8,
+    color: '#FFF'
+  },
+  button:{
+    width: '95%',
+    height: 40,
+    backgroundColor: '#3fffcf',
+    borderRadius: 4,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  buttonText:{
+   fontSize: 18, 
+   fontWeight: 'bold',
+   color: '#102623'
+  }
+})
